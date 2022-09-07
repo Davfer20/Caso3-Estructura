4e1131d20eae2a0fd8fa4f256314fd98cf80e969
Davfer20 <100215276+Davfer20@users.noreply.github.com>
2022-09-07 16:46:11 -0600

    Preview 3

--- Ej2/Main2.cpp
+++ Ej2/Main2.cpp
@@ -1,12 +1,9 @@
-#include "../Generic/Queue.h"
 #include "../Generic/Pila.h"
 
 using namespace std;
 
 int main()
 {
-    // string frase = "(110+5)*2";
-
     Pila<int> *pila1 = new Pila<int>();
 
     int datosPila[] = {2, 4, 7};
